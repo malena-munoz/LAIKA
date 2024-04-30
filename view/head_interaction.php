94538ce5d04b8a18561ea17497c8dc8c3020dc26
Malena Muñoz <160533284+malena-munoz@users.noreply.github.com>
2024-04-30 10:30:05 +0200

    Separación de NAV y FOOTER en archivos diferentes + inicio del header (sin terminar)

--- view/head_interaction.php
+++ view/head_interaction.php
@@ -1,3 +1,7 @@
-<head>
-    
-</head>
+<header>
+    <span class="material-symbols-rounded" id="home">home</span>
+    <input placeholder="Searth the internet..." type="text" name="text" id="search-input">
+    <div id="user-icon">
+        <img src="assets\img\uwu.png" alt="Icon of user">
+    </div>
+</header>
--- view/main_page.php
+++ view/main_page.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Welcome</title>
-        <link rel="stylesheet" href="assets/css/common_style.css"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0"/>
+        <link rel="stylesheet" href="assets/css/common_style.css"/>
     </head>
     <body>
         <!-- Etiqueta del audio actual, contiene el archivo MP3 -->
@@ -17,6 +17,8 @@
         <?php $activeLink = 'main_page'; include 'nav_playlists.php';?>
         <!-- Contenedor principal, irá cambiando conforme al programa -->
         <div id="main-divider">
+            <!-- Página del encabezado de navegación principal y otros controles que interaccionará el usuario -->
+            <?php include 'head_interaction.php';?>
             <h1>HOLA</h1>
         </div>
         <!-- Página del controlador de la canción actual -->

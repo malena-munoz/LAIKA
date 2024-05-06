bf8bbf30a911f64e83c7377a2c30a13208e76d5e
Malena Muñoz <160533284+malena-munoz@users.noreply.github.com>
2024-05-06 11:26:47 +0200

    Cambio radical en los ficheros PHP (reconstruidos desde 0), separación de CSS por archivo PHP y organización de archivos. NOTA: faltan algunos archivos CSS por comentar, para un mejor entendimiento.

---
+++ view/home.php
@@ -0,0 +1,17 @@
+<div class='main-content'>
+    <h3 style="margin: var(--global-separation) 0 2px 0;">Los albumes mas escuchados</h3>
+    <div id="albums-container" class="scroll-container">
+        <button id="albums-scroll-left" class="scroll-left">&lt;</button>
+        <div id="main-albums">
+        </div>
+        <button id="albums-scroll-right" class="scroll-right">&gt;</button>
+    </div>
+    <h3 style="margin: var(--global-separation) 0 2px 0;">Los artistas del momento</h3>
+    <div id="artists-container" class="scroll-container">
+        <button id="artists-scroll-left" class="scroll-left">&lt;</button>
+        <div id="main-artists">
+            
+        </div>
+        <button id="artists-scroll-right" class="scroll-right">&gt;</button>
+    </div>
+</div>
---
+++ view/nav.php
@@ -0,0 +1,34 @@
+<nav>
+    <div id="vertical-container">
+        <div id="scrollable-content">
+            <section class="filler"></section>
+            <ul id="playlist-list">
+                <li>
+                    <div class="playlist">1</div>
+                </li>
+                <li>
+                    <div class="playlist">2</div>
+                </li>
+                <li>
+                    <div class="playlist">3</div>
+                </li>
+                <li>
+                    <div class="playlist">4</div>
+                </li>
+                <li>
+                    <div class="playlist">5</div>
+                </li>
+                <li>
+                    <div class="playlist">6</div>
+                </li>
+                <li>
+                    <div class="playlist">7</div>
+                </li>
+            </ul>
+            <section class="filler"></section>
+        </div>
+        <div id="img-current-song">
+            <img src="assets\img\image.png" alt="Album of the playing song">
+        </div>
+    </div>
+</nav>

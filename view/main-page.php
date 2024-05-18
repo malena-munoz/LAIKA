232661b1eed9665c2538c6491bee882e71ff7ed9
Malena Muñoz <160533284+malena-munoz@users.noreply.github.com>
2024-05-18 13:02:42 +0200

    Muchos cambios jeje (detalles en descripción)
    - He ordenado la parte del home, en una carpeta llamada "landing"
    - Añadido la sección aleatoria del home (html, css y js)
    - Scroll horizontal estilizado
    - ARCHIVOS RENOMBRADOS
    - Cambios puntuales que no afectan a funcionalidad "en el texto, básicamente"
    - Suprimido algunos svg del botón de registrarte, eran innecesarios

--- view/main-page.php
+++ view/main-page.php
@@ -15,7 +15,7 @@ $_SESSION['loggedin'] = false; // Establecer la variable de sesión
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0"/>
         <link rel="stylesheet" href="assets/css/common-style.css"/>
         <link rel="stylesheet" href="assets/css/nav-style.css"/>
-        <link rel="stylesheet" href="assets/css/footer-style.css"/>
+        <link rel="stylesheet" href="assets/css/player-style.css"/>
         <link rel="stylesheet" href="assets/css/header-style.css"/>
         <link rel="stylesheet" href="assets/css/home-style.css"/>
         <link rel="stylesheet" href="assets/css/register-style.css"/>
@@ -26,16 +26,23 @@ $_SESSION['loggedin'] = false; // Establecer la variable de sesión
                 <?php include 'register.php'; ?>
             </div>
         </div>
+        <!-- Canción en reproducción -->
         <audio id="playing-song-audio" ontimeupdate="refreshProgressBar(), changeMaxValueSongProgress()">
             <source src="./assets/audio/paid.mp3" type="audio/mp3">
         </audio>
         <?php include 'nav.php';?>
         <div id="main-divider">
+            <!-- Barra de búsqueda -->
             <?php include 'header.php';?>
-            <?php include 'home.php';?>
+
+            <?php include 'landing/home-real.php';?>
+            <!-- Datos empresa -->
+            <!-- <php include 'home.php';?> -->
+            <!-- Sección de relleno -->
             <section class="filler"></section>
         </div>
-        <?php include 'footer.php';?>
+        <!-- Reproductor -->
+        <?php include 'player.php';?>
     </body>
     <script src="https://widget.deezer.com/widget/dynamic/deezer-widget-loader.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
